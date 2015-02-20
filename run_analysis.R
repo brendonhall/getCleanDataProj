556064d0182c92b11b05e206a8ac34e37abcc714
brendonhall <brendon.hall@gmail.com>
2015-02-20 16:30:13 -0600

    add code book

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 #Getting and Cleaning Data Class Project
-#install.packages("dplyr")
+
 list.of.packages <- c("dplyr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
